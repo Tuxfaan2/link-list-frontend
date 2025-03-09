51af61898af6501846cd02f136c93731a3f9aea0
Tuxfan <jonas@familie-wuepping.de>
2025-03-09 21:22:40 +0100

    Add logic for authorizing via token

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,15 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import OverviewView from "@/views/OverviewView.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import OverviewView from '@/views/OverviewView.vue';
+import LoginView from '@/views/LoginView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'overwiev',
+      name: 'overview',
       component: OverviewView,
     },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    },
   ],
-})
+});
 
-export default router
+export default router;
