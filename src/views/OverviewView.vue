eb95148a0c1e27856146f95bee8118421d3bbc48
Tuxfan <jonas@familie-wuepping.de>
2025-03-13 21:55:17 +0100

    Change smth

--- src/views/OverviewView.vue
+++ src/views/OverviewView.vue
@@ -91,6 +91,7 @@ function openCreateLinkPopup() {
 
 async function createListItem(req: CreateLinkItemRequest) {
   const newLinkItem = await createLinkItem(req);
+  console.log('created link');
   links.value.unshift(newLinkItem);
   closeModal();
 }
