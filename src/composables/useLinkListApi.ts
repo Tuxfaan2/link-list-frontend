51d6482964f3f867a5ed68348598c56124331c29
Tuxfan <mensch1906@gmail.com>
2025-12-23 00:54:02 +0100

    Add logic to migrate to ts-client

--- src/composables/useLinkListApi.ts
+++ src/composables/useLinkListApi.ts
@@ -1,51 +1,38 @@
-import type { CreateLinkItemRequest, LinkItem, MeilisearchLinkResponse } from '@/types/LinkTypes';
+import type { CreateLinkItemRequest, LinkItem } from '@/types/LinkTypes';
 import { useToken } from '@/composables/useToken';
+import { ref } from 'vue';
+import {
+  Configuration,
+  LinkApi,
+  MeilisearchApi,
+  type MeilisearchLinkSearchResponse,
+} from '@tuxfaan2/link-list-api';
 
 export function useLinkListApi() {
   const { getOrRefreshToken } = useToken();
+  const configuration = new Configuration({
+    accessToken: async () => {
+      return await getOrRefreshToken();
+    },
+    basePath: '/backend',
+  });
+
+  const meilisearchApiClient = ref<MeilisearchApi>(new MeilisearchApi(configuration));
+
+  const linkApiClient = ref<LinkApi>(new LinkApi(configuration));
 
   async function createLinkItem(req: CreateLinkItemRequest): Promise<LinkItem> {
-    const resp = await fetch('/backend/api/v1/link', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${await getOrRefreshToken()}`,
-      },
-      body: JSON.stringify(req),
-    });
-
-    return resp.json();
+    return linkApiClient.value.createLinkItem({ createLinkItemRequest: req });
   }
 
-  async function searchLinkItems(query: string): Promise<MeilisearchLinkResponse> {
-    const token = await getOrRefreshToken();
-    console.log(token);
-    const resp = await fetch('/backend/api/v1/search-link', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({ q: query }),
+  async function searchLinkItems(query: string): Promise<MeilisearchLinkSearchResponse> {
+    return await meilisearchApiClient.value.searchForLinks({
+      meilisearchSearchRequest: { q: query },
     });
-
-    return resp.json();
   }
 
   async function deleteLinkItem(linkId: number): Promise<LinkItem> {
-    const token = await getOrRefreshToken();
-    const resp = await fetch('/backend/api/v1/link/' + linkId, {
-      method: 'DELETE',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    return resp.json();
+    return linkApiClient.value.deleteLinkItem({ linkId });
   }
 
   return { createLinkItem, searchLinkItems, deleteLinkItem };
