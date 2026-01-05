99a174aac0990735265943a701adc448de12288e
tuxfaan2 <mensch1906@gmail.com>
2026-01-05 22:12:27 +0100

    Add paging-button

---
+++ src/components/PagingButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button class="size-fit" @click="onGoOnePageLeft">
+      <FontAwesomeIcon :icon="faArrowAltCircleLeft" />
+    </button>
+    <span>{{ currentPage }}/{{ totalPages }}</span>
+    <button class="size-fit" @click="onGoOnePageRight">
+      <FontAwesomeIcon :icon="faArrowAltCircleRight" />
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faArrowAltCircleLeft, faArrowAltCircleRight } from '@fortawesome/free-regular-svg-icons';
+
+defineProps<{
+  currentPage: number;
+  totalPages: number;
+}>();
+
+const emit = defineEmits<{ (event: 'on-go-left'): void; (event: 'on-go-right'): void }>();
+
+function onGoOnePageRight() {
+  emit('on-go-right');
+}
+
+function onGoOnePageLeft() {
+  emit('on-go-left');
+}
+</script>
+
+<style scoped></style>
